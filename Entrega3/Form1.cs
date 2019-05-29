b76dd16660f7b0a66795ec897e79fc335c748e9f
GianfrancoTP <gstraverso@miuandes.cl>
2019-05-29 17:38:51 -0400

    init

---
+++ Entrega3/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Entrega3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            int alto;
+            int ancho;
+            int cantidad_mese;
+            Terreno Vegetación = new Terreno("Vegetacn");
+            Terreno Acuático = new Terreno("Acuatico");
+            Terreno Desierto = new Terreno("Desierto");
+            Terreno Nieve = new Terreno("NieveIce");
+            Terreno Volcán = new Terreno("Volcanic");
+        }
+    }
+}
