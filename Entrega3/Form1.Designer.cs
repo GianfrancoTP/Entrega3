b4622edb04cd9dc7587cfc2def3d1450043b797c
itmayer <itmayer@miuandes.cl>
2019-05-29 20:11:38 -0400

    first commit

--- Entrega3/Form1.Designer.cs
+++ Entrega3/Form1.Designer.cs
@@ -29,38 +29,73 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.ColumnCount = 10;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(107, 91);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1752, 860);
+            this.tableLayoutPanel1.RowCount = 10;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(571, 325);
             this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(312, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bitmonlandia";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1776, 1254);
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(945, 515);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Entrega3/Form1.cs
+++ Entrega3/Form1.cs
@@ -7,22 +7,284 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Linq;
 
 namespace Entrega3
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
-            int alto;
-            int ancho;
-            int cantidad_mese;
+            List<Bitmon> bitmons1 = new List<Bitmon>();
+
+            Mapa mapa1 = new Mapa();
             Terreno Vegetación = new Terreno("Vegetacn");
             Terreno Acuático = new Terreno("Acuatico");
             Terreno Desierto = new Terreno("Desierto");
             Terreno Nieve = new Terreno("NieveIce");
             Terreno Volcán = new Terreno("Volcanic");
+            // Creamos la matriz de terrenos predefinida
+            Terreno[,] opcion1 = new Terreno[10, 10];
+            opcion1[0, 0] = Desierto;
+            opcion1[1, 0] = Desierto;
+            opcion1[1, 1] = Desierto;
+            opcion1[1, 2] = Desierto;
+            opcion1[1, 3] = Desierto;
+            opcion1[1, 4] = Desierto;
+            opcion1[1, 5] = Desierto;
+            opcion1[1, 6] = Desierto;
+            opcion1[2, 3] = Desierto;
+            opcion1[2, 4] = Desierto;
+            opcion1[2, 5] = Desierto;
+            opcion1[2, 6] = Desierto;
+            opcion1[2, 7] = Desierto;
+            opcion1[3, 6] = Desierto;
+            opcion1[3, 7] = Desierto;
+            opcion1[3, 8] = Desierto;
+            opcion1[3, 9] = Desierto;
+            opcion1[4, 5] = Desierto;
+            opcion1[4, 6] = Desierto;
+            opcion1[4, 7] = Desierto;
+            opcion1[4, 8] = Desierto;
+            opcion1[4, 9] = Desierto;
+            opcion1[5, 6] = Desierto;
+            opcion1[5, 8] = Desierto;
+            opcion1[0, 1] = Acuático;
+            opcion1[0, 1] = Acuático;
+            opcion1[0, 2] = Acuático;
+            opcion1[0, 3] = Acuático;
+            opcion1[0, 4] = Acuático;
+            opcion1[0, 5] = Acuático;
+            opcion1[5, 7] = Acuático;
+            opcion1[5, 9] = Acuático;
+            opcion1[6, 5] = Acuático;
+            opcion1[6, 6] = Acuático;
+            opcion1[6, 7] = Acuático;
+            opcion1[6, 8] = Acuático;
+            opcion1[6, 9] = Acuático;
+            opcion1[7, 1] = Acuático;
+            opcion1[7, 4] = Acuático;
+            opcion1[7, 5] = Acuático;
+            opcion1[7, 9] = Acuático;
+            opcion1[8, 0] = Acuático;
+            opcion1[8, 1] = Acuático;
+            opcion1[8, 2] = Acuático;
+            opcion1[8, 3] = Acuático;
+            opcion1[8, 4] = Acuático;
+            opcion1[9, 0] = Acuático;
+            opcion1[9, 1] = Acuático;
+            opcion1[9, 2] = Acuático;
+            opcion1[9, 3] = Acuático;
+            opcion1[0, 6] = Nieve;
+            opcion1[0, 7] = Nieve;
+            opcion1[0, 8] = Nieve;
+            opcion1[0, 9] = Nieve;
+            opcion1[1, 7] = Nieve;
+            opcion1[1, 8] = Nieve;
+            opcion1[1, 9] = Nieve;
+            opcion1[2, 8] = Nieve;
+            opcion1[2, 9] = Nieve;
+            opcion1[2, 0] = Vegetación;
+            opcion1[2, 1] = Vegetación;
+            opcion1[2, 2] = Vegetación;
+            opcion1[3, 0] = Vegetación;
+            opcion1[3, 1] = Vegetación;
+            opcion1[3, 2] = Vegetación;
+            opcion1[3, 3] = Vegetación;
+            opcion1[3, 4] = Vegetación;
+            opcion1[3, 5] = Vegetación;
+            opcion1[4, 0] = Vegetación;
+            opcion1[4, 1] = Vegetación;
+            opcion1[4, 2] = Vegetación;
+            opcion1[4, 3] = Vegetación;
+            opcion1[4, 4] = Vegetación;
+            opcion1[5, 0] = Vegetación;
+            opcion1[5, 1] = Vegetación;
+            opcion1[5, 2] = Vegetación;
+            opcion1[5, 3] = Vegetación;
+            opcion1[5, 4] = Vegetación;
+            opcion1[5, 5] = Vegetación;
+            opcion1[6, 0] = Vegetación;
+            opcion1[6, 1] = Vegetación;
+            opcion1[6, 2] = Vegetación;
+            opcion1[6, 3] = Vegetación;
+            opcion1[6, 4] = Vegetación;
+            opcion1[7, 0] = Vegetación;
+            opcion1[7, 2] = Vegetación;
+            opcion1[7, 3] = Vegetación;
+            opcion1[7, 6] = Volcán;
+            opcion1[7, 7] = Volcán;
+            opcion1[7, 8] = Volcán;
+            opcion1[8, 5] = Volcán;
+            opcion1[8, 6] = Volcán;
+            opcion1[8, 7] = Volcán;
+            opcion1[8, 8] = Volcán;
+            opcion1[8, 9] = Volcán;
+            opcion1[9, 4] = Volcán;
+            opcion1[9, 5] = Volcán;
+            opcion1[9, 6] = Volcán;
+            opcion1[9, 7] = Volcán;
+            opcion1[9, 8] = Volcán;
+            opcion1[9, 9] = Volcán;
+
+            //Creamos la poblacion inicial de Bitmon
+
+            //Poblacion inicial de Taplan
+            int[] pos1 = { 2, 2 };
+            Taplan taplan1 = new Taplan(pos1);
+            int[] pos2 = { 4, 1 };
+            Taplan taplan2 = new Taplan(pos2);
+            int[] pos3 = { 4, 2 };
+            Taplan taplan3 = new Taplan(pos3);
+            int[] pos4 = { 5, 3 };
+            Taplan taplan4 = new Taplan(pos4);
+            int[] pos5 = { 6, 0 };
+            Taplan taplan5 = new Taplan(pos5);
+
+            //Poblacion inicial de Ents
+            int[] pos6 = { 2, 4 };
+            Ent Ent1 = new Ent(pos6);
+            int[] pos7 = { 0, 9 };
+            Ent Ent2 = new Ent(pos7);
+            int[] pos8 = { 3, 9 };
+            Ent Ent3 = new Ent(pos8);
+            int[] pos9 = { 4, 4 };
+            Ent Ent4 = new Ent(pos9);
+
+            //Poblacion inicial de Wetar
+            int[] pos10 = { 0, 5 };
+            Wetar Wetar1 = new Wetar(pos10);
+            int[] pos11 = { 0, 1 };
+            Wetar Wetar2 = new Wetar(pos11);
+            int[] pos12 = { 9, 0 };
+            Wetar Wetar3 = new Wetar(pos12);
+
+            //Poblacion inicial de Gofue
+            int[] pos13 = { 1, 7 };
+            Gofue Gofue1 = new Gofue(pos13);
+            int[] pos14 = { 7, 7 };
+            Gofue Gofue2 = new Gofue(pos14);
+            int[] pos15 = { 9, 5 };
+            Gofue Gofue3 = new Gofue(pos15);
+
+            //Poblacion inicial Dorvalo
+            int[] pos16 = { 1, 7 };
+            Dorvalo Dorvalo1 = new Dorvalo(pos16);
+            int[] pos17 = { 3, 7 };
+            Dorvalo Dorvalo2 = new Dorvalo(pos17);
+            int[] pos18 = { 2, 7 };
+            Dorvalo Dorvalo3 = new Dorvalo(pos18);
+
+            //Poblacion inicial Doti
+            int[] pos19 = { 1, 5 };
+            Doti Doti1 = new Doti(pos19);
+            int[] pos20 = { 9, 7 };
+            Doti Doti2 = new Doti(pos20);
+            int[] pos21 = { 0, 0 };
+            Doti Doti3 = new Doti(pos21);
+            int[] pos22 = { 5, 7 };
+            Doti Doti4 = new Doti(pos22);
+
+            bitmons1.Add(taplan1);
+            bitmons1.Add(taplan2);
+            bitmons1.Add(taplan3);
+            bitmons1.Add(taplan4);
+            bitmons1.Add(taplan5);
+            bitmons1.Add(Ent1);
+            bitmons1.Add(Ent2);
+            bitmons1.Add(Ent3);
+            bitmons1.Add(Ent4);
+            bitmons1.Add(Wetar1);
+            bitmons1.Add(Wetar2);
+            bitmons1.Add(Wetar3);
+            bitmons1.Add(Gofue1);
+            bitmons1.Add(Gofue2);
+            bitmons1.Add(Gofue3);
+            bitmons1.Add(Dorvalo1);
+            bitmons1.Add(Dorvalo2);
+            bitmons1.Add(Dorvalo3);
+            bitmons1.Add(Doti1);
+            bitmons1.Add(Doti2);
+            bitmons1.Add(Doti3);
+            bitmons1.Add(Doti4);
+
+            mapa1.Alto = 10;
+            mapa1.Ancho = 10;
+            mapa1.Terrenos = opcion1;
+            mapa1.Bitmons = bitmons1;
+
+            int alto = 10;
+            int ancho = 10;
+            int cantidad_mese;
+ 
+            Label[,] labels = new Label[alto,ancho];
+            for (int i = 0; i < alto; i++)
+            {
+                for (int j = 0; j < ancho; j++)
+                {
+                    Label label = new Label();
+                    label.AutoSize = true;
+                    label.Dock = DockStyle.Fill;
+                    label.Location = new Point(3, 0);
+                    label.Name = "label";
+                    label.Size = new Size(51, 32);
+                    label.TabIndex = 0;
+                    string str = "";
+                    var bitmons = mapa1.Bitmons.Where(x => (x.Posicion[0] == i) && (x.Posicion[1] == j)).ToList();
+                    foreach (var bitmon in bitmons)
+                    {
+                        str += bitmon.Tipo;
+                        str += ' ';
+                    }
+                    label.Text = str;
+                    if (opcion1[i,j].tipo == "Vegetacn")
+                    {
+                        label.BackColor = Color.Green;
+                    }
+                    else if (opcion1[i, j].tipo == "Acuatico")
+                    {
+                        label.BackColor = Color.Blue;
+                    }
+                    else if (opcion1[i, j].tipo == "Desierto")
+                    {
+                        label.BackColor = Color.SandyBrown;
+                    }
+                    else if (opcion1[i, j].tipo == "NieveIce")
+                    {
+                        label.BackColor = Color.Snow;
+                    }
+                    else
+                    {
+                        label.BackColor = Color.DarkRed;
+                    }
+                    tableLayoutPanel1.Controls.Add(label, j, i);
+                    labels[i, j] = label;
+                }
+            }
+        }
+
+        
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
